a7ee68b77a1c04a77afb86bea14e1dda285ced9f
Jayden Nguyen <jayden.m.nguyen@student.unsw.edu.au>
2024-06-26 23:26:22 +1000

    added upload script and necessary libraries, as well as screenshots

--- exercises/databases/myGarregMach/server/uploadData.js
+++ exercises/databases/myGarregMach/server/uploadData.js
@@ -1,12 +1,38 @@
 import { readFileSync } from 'fs';
 import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
+import { exit } from 'process';
 
 const dbData = readFileSync('./dbData.json', 'utf-8');
 const jsonData = JSON.parse(dbData);
 
-const apiConfig = JSON.parse(readFileSync('./fireBaseConfig.json', 'utf-8'));
+const { apiKey, authDomain, projectId, storageBucket, messagingSenderId, appId } = JSON.parse(readFileSync('./fireBaseConfig.json', 'utf-8'));
+
+const app = initializeApp({
+    apiKey, authDomain, projectId, storageBucket, messagingSenderId, appId
+});
+
+const db = getFirestore(app);
 
 
+async function pushData() {
+    try {
+        for (const col of Object.keys(jsonData)) {
+            const newCollect = collection(db, col);
+    
+            for (const data of jsonData[col]) {
+                const newDoc = await addDoc(newCollect, data);
+                console.log('Document written with ID: ', newDoc.id);
+            }
+        }
 
-console.log(apiConfig.apiKey);
+    } catch (e) {
+        console.error('Error adding document: ', e);
+    }
+
+}
+
+await pushData();
+console.log('FINISHED UPLOADING!');
+exit(0);
 
