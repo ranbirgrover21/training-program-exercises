e672b0842e67ad73a095c6af38fcb8f4d8664678
Jayden Nguyen <jayden.m.nguyen@student.unsw.edu.au>
2024-06-27 00:28:31 +1000

    setup backend

--- exercises/databases/myGarregMach/server/src/index.ts
+++ exercises/databases/myGarregMach/server/src/index.ts
@@ -1,1 +1,18 @@
-console.log('slay');
+
+import express, { Request, Response } from 'express';
+import { SERVER_PORT } from '../../config.json';
+
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    console.log('Hello, TypeScript with Express :)))!');
+    res.send('Hello, TypeScript with Express :)))!');
+});
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Server running on port http://localhost:${SERVER_PORT}`);
+});
