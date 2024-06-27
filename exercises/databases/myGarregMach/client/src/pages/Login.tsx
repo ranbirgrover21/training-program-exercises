22f03a05cccfd00f4100585c0076fda865d992e4
Jayden Nguyen <jayden.m.nguyen@student.unsw.edu.au>
2024-06-27 20:03:17 +1000

    added server changes

--- exercises/databases/myGarregMach/client/src/pages/Login.tsx
+++ exercises/databases/myGarregMach/client/src/pages/Login.tsx
@@ -6,45 +6,43 @@ import { useNavigate } from "react-router";
 import { Alert } from "@mui/material";
 
 export function Login() {
-    const [gID, setGID] = useState("");
-    const [password, setPassword] = useState("");
-    const [displayError, setError] = useState(false);
-    const navigate = useNavigate();
-    
-    function handleSubmit() {
-        setError(!displayError);
+  const [gID, setGID] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayError, setError] = useState(false);
+  const navigate = useNavigate();
+  
+  function handleSubmit() {
+    setError(!displayError);
 
-        console.log(gID);
-        localStorage.setItem("gId", gID);
-        localStorage.setItem('USER_TYPE', '');
-        console.log(password);
-        navigate("/home");
-    }
- 
-    return (
-        <>
-            <Navbar/>
-            
-            <div className="flex w-screen h-[87%] padding-2 justify-center items-center">
+    console.log(gID);
+    localStorage.setItem("gId", gID);
+    localStorage.setItem('USER_TYPE', '');
+    console.log(password);
+    navigate("/home");
+  }
 
-                <div className="flex flex-col items-center py-20 w-[70%] h-[80%] shadow-lg bg-[url('./src/assets/MAIN_BLOCK.png')] overflow-scroll">
-                    <h1 className="text-4xl text-center">Welcome Back!</h1>
-                    { displayError ? <Alert variant="filled" severity="error" className="mt-2">Failed to login</Alert> : null}
-                    <div className="flex flex-col items-center text-left w-[70%] h-[50%] mt-8 ">
-                        <div className="w-[75%] mb-[4%]">
-                            <h2 className="text-xl">gID</h2>
-                            <input type="text" className="w-full shadow-xl p-[10px] loginInput" onChange={e => setGID(e.target.value)}></input>
-                        </div>
-                        <div className="w-[75%]">
-                            <h2 className="text-xl">Password</h2>
-                            <input type="password" className="w-full shadow-xl p-[10px] mb-7 loginInput"  onChange={e => setPassword(e.target.value)}></input>
-                        </div>
-                    </div>
-                    <div className="flex justify-center items-center w-[70%] h-[20%] mt-8">
-                        <GMButton name="Log In" onClick={handleSubmit}/>
-                    </div>
-                </div>
+  return (
+    <>
+      <Navbar/>
+        <div className="flex w-screen h-[87%] padding-2 justify-center items-center">
+          <div className="flex flex-col items-center py-20 w-[70%] h-[80%] shadow-lg bg-[url('./src/assets/MAIN_BLOCK.png')] overflow-scroll">
+            <h1 className="text-4xl text-center">Welcome Back!</h1>
+            { displayError ? <Alert variant="filled" severity="error" className="mt-2">Failed to login</Alert> : null}
+            <div className="flex flex-col items-center text-left w-[70%] h-[50%] mt-8 ">
+              <div className="w-[75%] mb-[4%]">
+                <h2 className="text-xl">gID</h2>
+                <input type="text" className="w-full shadow-xl p-[10px] loginInput" onChange={e => setGID(e.target.value)}></input>
+              </div>
+              <div className="w-[75%]">
+                <h2 className="text-xl">Password</h2>
+                <input type="password" className="w-full shadow-xl p-[10px] mb-7 loginInput"  onChange={e => setPassword(e.target.value)}></input>
+              </div>
             </div>
-        </>
-    )
+            <div className="flex justify-center items-center w-[70%] h-[20%] mt-8">
+              <GMButton name="Log In" onClick={handleSubmit}/>
+            </div>
+          </div>
+        </div>
+    </>
+  )
 }
--- exercises/databases/myGarregMach/server/src/index.ts
+++ exercises/databases/myGarregMach/server/src/index.ts
@@ -1,7 +1,9 @@
 
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { SERVER_PORT } from '../../config.json';
+import { login } from './funcs/login';
 
+const errorHandler = require('http-errors-middleware');
 const cors = require('cors');
 const app = express();
 
@@ -9,10 +11,24 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
-    console.log('Hello, TypeScript with Express :)))!');
-    res.send('Hello, TypeScript with Express :)))!');
+  console.log('Hello, TypeScript with Express :)))!');
+  res.send('Hello, TypeScript with Express :)))!');
 });
 
+app.post('/login', async (req: Request, res: Response, next : NextFunction) => {
+  const { gID, password } = req.body;
+  
+  try {
+    const d = await login(gID, password);
+    res.send(d);
+  } catch (e) {
+    next(e);
+  }
+
+});
+
+app.use(errorHandler( { debug : true }));
+
 app.listen(SERVER_PORT, () => {
-    console.log(`Server running on port http://localhost:${SERVER_PORT}`);
+  console.log(`Server running on port http://localhost:${SERVER_PORT}`);
 });
