46b07a2dbc399026a1d48a615c9282443f215105
Hanyuan Li <work@hanyuone.liv>
2024-09-01 13:00:29 +1000

    docs: add documentation to Highlight.tsx

--- exercises/idle-game/src/components/Highlight.tsx
+++ exercises/idle-game/src/components/Highlight.tsx
@@ -4,6 +4,14 @@ import { Line } from "../types";
 
 import "highlight.js/styles/github.css";
 
+/**
+ * A component that adds syntax highlighting to code. Used in the game `scrip.ts`
+ * to show new lines of code being "written" as part of the idle game.
+ * @param line - a struct that contains details both the `contents` of the code
+ * snippet that we want to highlight, and the `language` the code snippet is
+ * written in (e.g. `js`, `css`, `py`).
+ * @returns 
+ */
 export default function Highlight({ language, contents }: Line) {
   const codeRef = useRef<HTMLElement | null>(null);
 
